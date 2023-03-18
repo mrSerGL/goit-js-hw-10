b570f7e5a2f50b6e0d67a835e38c3994511d9aff
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-03-18 09:59:00 +0200

    add debounce

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,28 @@
 import '../css/styles.css';
+import debounce from 'lodash.debounce';
+
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  inputField: document.querySelector('#search-box'),
+};
+
+refs.inputField.addEventListener(
+    'input',
+    debounce(() => {
+      takeRequestData();
+    }, DEBOUNCE_DELAY)
+  );
+
+function takeRequestData(event) {
+  console.log(refs.inputField.value.trim());
+}
+
+
+
+
+
+
+
+
