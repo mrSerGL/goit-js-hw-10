7626745262baab0e258d07ec941429f2a4e24b71
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-03-18 19:27:24 +0200

    add country info

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,7 @@ const countriesApiService = new CountriesApiService();
 const refs = {
   inputField: document.querySelector('#search-box'),
   countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
 };
 
 refs.inputField.addEventListener(
@@ -22,19 +23,27 @@ refs.inputField.addEventListener(
 function getCountriesInfo(event) {
   countriesApiService.query = event.target.value.trim();
   countriesApiService.fetchCountries().then(countries => {
-    checkReceivedData(countries);
+  
     createMarkup(countries);
+    checkReceivedData(countries);
   });
 }
 
 function checkReceivedData(countries) {
   const foundCountries = Number(countries.length);
 
+  refs.countryInfo.innerHTML = '';
+
   if (countries.status === 404) {
     Notify.failure('Oops, there is no country with that name');
   } else if (foundCountries > 10) {
     Notify.info('Too many matches found. Please enter a more specific name');
-  }
+  } else if (foundCountries === 1){
+    console.log('FOUND!');
+    refs.countryList.innerHTML = '';
+    createCountryInfo(countries);
+    } 
+
   console.log(countries);
 }
 
@@ -42,15 +51,33 @@ function createMarkup(countries) {
   refs.countryList.innerHTML = '';
 
   const markup = countries
-  .map((country, index) => {
-    const { flags, name} = countries[index];
+    .map((country, index) => {
+      const { flags, name } = countries[index];
 
-    return `<li>
+      return `<li>
     <img class="flag" width="20px" height="20px" src="${flags.svg}" alt="">
     <span class="name" >${name.common}</span>
     </li>`;
-  }
-  ).join('');
+    })
+    .join('');
 
   refs.countryList.innerHTML = markup;
 }
+
+
+
+function createCountryInfo(countries) {
+  const { flags, name } = countries[0];
+
+  const markup = `
+  <img class="flag" width="40px" height="40px" src="${flags.svg}" alt="">
+  <span class="name" >${name.common}</span>`;
+
+
+   refs.countryInfo.innerHTML = markup;
+
+
+
+ };
+
+
